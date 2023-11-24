e82b79ecb35469f8e0663b0717c65fa21c497a2e
Mecoli <mecoli@Mecoli-MacBook-Pro.local>
2023-11-24 02:45:46 -0500

    modify description

--- components/About/Description.tsx
+++ components/About/Description.tsx
@@ -15,14 +15,17 @@ export default function Description() {
                     height={500} />
             </div>
             <div className="text-base text-gray-500 dark:text-gray-300 indent-8 text-left sm:basis-3/5 xl:basis-4/6">
-                <div className="pb-5 ">
-                    Hello! My name is <b>Chun-Mao Lai (賴群貿)</b>, and I am also known as <b>Michael</b> or <b>Mecoli</b>.
+                <div className="pb-3">
+                    Hello! My name is <b>Chun-Mao Lai (賴群貿)</b>, and I am also known as <b>Michael</b> or <b>Mecoli</b>. My personal interests include playing piano, composing music, and building websites.
                 </div>
-                <div className="pb-5 ">
-                    I was born in Tainan, Taiwan, and I am currently an undergraduate student in the Electrical Engineering (EE) Department at National Taiwan University (NTU) since 2020. My primary interest is in building user-friendly applications and integrating machine learning techniques to improve the quality of life.
+                <div className="pb-3">
+                    I am currently an undergraduate student in the Electrical Engineering (EE) Department at National Taiwan University (NTU) since 2020. My primary interest is in building user-friendly applications and integrating machine learning techniques to improve the quality of life.
                 </div>
-                <div className="pb-5 ">
+                <div className="pb-3">
                     My research interests mainly focus on Deep Learning, including Reinforcement Learning, Natural Language Progressing, and Computer Vision. During my undergraduate study, I have actively participated in several laboratories and research projects, including <a className=" text-blue-500 dark:text-blue-300 font-bold hover:underline" href="https://nturobotlearninglab.github.io/dbc/">Robot Learning Lab</a>, <a className=" text-blue-500 dark:text-blue-300 font-bold hover:underline" href="https://www.csie.ntu.edu.tw/~miulab/">Machine Intelligence & Understanding Laboratory</a>, and <a className=" text-blue-500 dark:text-blue-300 font-bold hover:underline" href="https://speech.ee.ntu.edu.tw/~hylee/index.php">Speech Processing and Machine Learning Lab</a>.
+                </div>
+                <div className="">
+                    Fun Fact: &apos;Mecoli&apos; is a playful fusion of Michael Lai&apos;s pronunciation and the name of the bacteria E.coli.
                 </div>
             </div>
             {/* Image Part */}
